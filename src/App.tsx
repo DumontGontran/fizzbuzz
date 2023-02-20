5d609b56aac245acc40c4cbf7f05b742063a70f1
DumontGontran <dumont.gontran@orange.fr>
2023-02-21 00:00:49 +0100

    add input to choose positive number

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { KeyboardEvent, useState } from 'react';
 import './App.css';
 import FizzBuzz from './components/FizzBuzz';
 
@@ -12,6 +12,16 @@ function App() {
         <div className='buttons'>
           <button className='button' type='button' onClick={() => setCounter(counter - 1)} disabled={counter <= 0}>-</button>
           <button className='button' type='button' onClick={() => setCounter(counter + 1)}>+</button>
+        </div>
+        <div className='numberChoice'>
+          <label htmlFor="counter">Enter a positive number: </label>
+          <input type="number" name="counter" id="counter" min="0" step="1" value={counter} onChange={((event: any) => {
+            if(event.target.value >= 0) {
+              setCounter(event.target.value)
+            } else{
+              event.preventDefault();
+            }
+            })} />
         </div>
       </form>
     </div>
